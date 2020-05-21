8b20fe65103b87717128098570ba6acfc4b954ef
Nataliia Mogylchuk <nataliia.mogylchuk@gmail.com>
2020-05-21 13:03:12 +0300

    Implement welcome page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,16 @@ import { AppRoutingModule } from "./app-routing.module";
 import { MaterialModule } from "./shared/material/material.module";
 import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
 import { LoginFormComponent } from "./modules/home/components/login-form/login-form.component";
-
 import { ErrorInterceptor } from "./core/interceptors/error.interceptor";
 import { JwtInterceptor } from "./core/interceptors/jwt.interceptor";
 import { AlertComponent } from "./modules/home/components/alert/alert.component";
 import { WelcomeComponent } from './modules/home/components/welcome/welcome.component';
 import { NotFoundComponent } from './modules/home/components/not-found/not-found.component';
+import { HeaderComponent } from './modules/home/components/header/header.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +21,8 @@ import { NotFoundComponent } from './modules/home/components/not-found/not-found
     LoginFormComponent,
     AlertComponent,
     WelcomeComponent,
-    NotFoundComponent],
+    NotFoundComponent,
+    HeaderComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
