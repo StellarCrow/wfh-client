5fe224ae18f875473ed3f770a57119a9ec87a184
Daria <dariadar12@gmail.com>
2020-05-22 14:09:08 +0300

    moving background test

--- src/app/modules/game/components/pages/join-room/join-room.component.ts
+++ src/app/modules/game/components/pages/join-room/join-room.component.ts
@@ -7,5 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JoinRoomComponent {
 
+  public moveBackground(event: any) {
+    const element = event.target as HTMLElement;
+    element.style.backgroundPositionX = -event.offsetX / 15 + 'px';
+    element.style.backgroundPositionY = -event.offsetY / 15 + 'px';
+    element.style.backgroundSize = 'cover';
+    console.log(event);
+  }
+
 
 }
