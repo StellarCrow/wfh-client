06c306da66cfd3b56e519a45ebf84b57c0bf06e3
Dan <oddestdan@gmail.com>
2020-05-22 19:09:57 +0300

    [Fix] Improve code readability of module imports

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -1,39 +1,29 @@
-import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
-import { MatCardModule } from "@angular/material/card";
-import { MatTabsModule } from "@angular/material/tabs";
-import { MatInputModule } from "@angular/material/input";
-import { MatButtonModule } from "@angular/material/button";
-import { MatCheckboxModule } from "@angular/material/checkbox";
-import { MatIconModule } from "@angular/material/icon";
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { MatListModule } from "@angular/material/list";
-import { MatSidenavModule } from "@angular/material/sidenav";
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
+const modules = [
+  FormsModule,
+  MatCardModule,
+  MatTabsModule,
+  MatInputModule,
+  MatButtonModule,
+  MatCheckboxModule,
+  MatIconModule,
+  MatToolbarModule,
+  MatListModule,
+  MatSidenavModule,
+];
 @NgModule({
-  imports: [
-    FormsModule,
-    MatCardModule,
-    MatTabsModule,
-    MatInputModule,
-    MatButtonModule,
-    MatCheckboxModule,
-    MatIconModule,
-    MatToolbarModule,
-    MatListModule,
-    MatSidenavModule
-  ],
-  exports: [
-    FormsModule,
-    MatCardModule,
-    MatTabsModule,
-    MatInputModule,
-    MatButtonModule,
-    MatCheckboxModule,
-    MatIconModule,
-    MatToolbarModule,
-    MatListModule,
-    MatSidenavModule
-  ]
+  imports: [...modules],
+  exports: [...modules],
 })
 export class MaterialModule {}
