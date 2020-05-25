dbdf66c778c52c28ce021407dac57d2dfbe7ac04
Daria <dariadar12@gmail.com>
2020-05-25 17:48:48 +0300

    remove cors

--- server.js
+++ server.js
@@ -2,9 +2,6 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-const cors = require("cors");
-app.use(cors());
-
 app.use(express.static(__dirname + '/dist/wfh-client'));
 
 app.get('/*', function (req, res) {
