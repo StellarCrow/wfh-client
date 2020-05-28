42ac19e754a9c00984141db9c8eadf8484eb8244
max <maxonchil@gmail.com>
2020-05-28 17:41:58 +0300

    display username from new server serponse

--- src/app/modules/game/pages/game/game.component.ts
+++ src/app/modules/game/pages/game/game.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit, OnDestroy } from "@angular/core";
-import { GameViewService } from "../../services/game-view.service";
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { GameViewService } from '../../services/game-view.service';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 
 @Component({
-  selector: "app-game",
-  templateUrl: "./game.component.html",
-  styleUrls: ["./game.component.scss"],
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.scss'],
 })
 export class GameComponent implements OnInit, OnDestroy {
   currentView: string;
