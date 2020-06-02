0e1ad8d8911410c4e75777fc09e62ba1145438d1
uzumaki <zholud92@gmail.com>
2020-06-02 16:21:27 +0300

    test

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -15,7 +15,7 @@ exports.config = {
   capabilities: {
     browserName: 'chrome',
     chromeOptions: {
-      args: ['--headless', '--no-sandbox']
+      args: ['-headless', '--disable-gpu', '--window-size=800,600']
     }
   },
   directConnect: true,
--- karma.conf.js
+++ karma.conf.js
@@ -23,7 +23,7 @@ module.exports = function (config) {
     },
     reporters: ['progress', 'kjhtml'],
     customLaunchers: {
-      ChromeHeadlessCI: {
+      ChromeHeadless: {
         base: 'ChromeHeadless',
         flags: ['--no-sandbox']
       }
