6cd490391f242c6f1040642d2e5ed2ebdd8032db
Dan <oddestdan@gmail.com>
2020-06-02 16:53:18 +0300

    Create and arrange Tee component to display phrase and picture

--- src/app/modules/game/components/tee-image/tee-image.component.ts
+++ src/app/modules/game/components/tee-image/tee-image.component.ts
@@ -7,12 +7,4 @@ import { Component, Input } from '@angular/core';
 })
 export class TeeImageComponent{
   @Input() fillColor: string;
-
-  changeColor() {
-    const r = Math.floor(Math.random() * 256);
-    const g = Math.floor(Math.random() * 256);
-    const b = Math.floor(Math.random() * 256);
-    this.fillColor = `rgb(${r}, ${g}, ${b})`;
-  }
-
 }
