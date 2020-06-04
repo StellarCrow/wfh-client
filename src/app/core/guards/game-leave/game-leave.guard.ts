dd49d4f373eacd616d75225e64a0cfe0c2b94710
Daria <dariadar12@gmail.com>
2020-06-05 00:21:58 +0300

    add removing from room

--- src/app/core/guards/game-leave/game-leave.guard.ts
+++ src/app/core/guards/game-leave/game-leave.guard.ts
@@ -3,21 +3,26 @@ import {ActivatedRouteSnapshot, CanDeactivate, RouterStateSnapshot} from '@angul
 import {MatDialog} from '@angular/material/dialog';
 import {ModalLeavePage} from '../../../shared/components/modal-leave-page/modal-leave-page';
 import {tap} from 'rxjs/operators';
-import {Observable, ObservedValueOf} from 'rxjs';
+import {Observable} from 'rxjs';
+import {SocketService} from '../../../modules/game/services/socket.service';
+import {DataStoreService} from '../../services/data-store.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameLeaveGuard implements CanDeactivate<Observable<any>> {
 
-  constructor(private dialog: MatDialog) {
+  constructor(private dialog: MatDialog, private socketService: SocketService, private dataStore: DataStoreService) {
   }
 
   canDeactivate(component: any, currentRoute: ActivatedRouteSnapshot,
                 currentState: RouterStateSnapshot, nextState?: RouterStateSnapshot) {
     const dialogRef = this.dialog.open(ModalLeavePage, { panelClass: 'custom-dialog', maxWidth: '40%' });
     return dialogRef.afterClosed().pipe(
-      tap(() => {
+      tap((answer) => {
+        if (answer) {
+          this.socketService.emit('leave-room', { room: this.dataStore.roomCode, username: this.dataStore.getUserName(), });
+        }
         console.log('close dialog');
       })
     );
