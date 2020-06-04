ea04e023b10f186954774e6634c58125544878b1
Dan <oddestdan@gmail.com>
2020-06-04 17:22:57 +0300

    WIP Tie BE events logic with TeeVote component

--- src/app/modules/game/components/timer/timer.component.ts
+++ src/app/modules/game/components/timer/timer.component.ts
@@ -4,7 +4,7 @@ import {takeUntil} from 'rxjs/operators';
 import {SocketService} from '../../services/socket.service';
 import {DataStoreService} from '../../../../core/services/data-store.service';
 import {GameViewService} from '../../services/game-view.service';
-import {MATCHING, PHRASE, TEE_VOTE} from '../../constants/game-views';
+import {MATCHING, PHRASE, TEE_VOTE, TEE_RESULT} from '../../constants/game-views';
 import {Stages} from '../../constants/stages.enum';
 
 @Component({
@@ -33,6 +33,7 @@ export class TimerComponent implements OnInit, OnDestroy {
     this.listenStopEvent('stop-painting', PHRASE);
     this.listenStopEvent('stop-phrases', MATCHING);
     this.listenStopEvent('stop-matching', TEE_VOTE);
+    this.listenStopEvent('stop-voting', TEE_RESULT);
   }
 
   startCount(): void {
--- src/app/modules/game/pages/game/game-views/tee-vote-view/tee-vote-view.component.ts
+++ src/app/modules/game/pages/game/game-views/tee-vote-view/tee-vote-view.component.ts
@@ -1,18 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { GameViewService } from 'src/app/modules/game/services/game-view.service';
-import * as views from 'src/app/modules/game/constants/game-views.js';
-
 
 import {DataStoreService} from '../../../../../../core/services/data-store.service';
 import {Stages} from '../../../../constants/stages.enum';
-import {IPicture} from 'src/app/modules/game/interfaces/ipicture';
-import {IPhrase} from 'src/app/modules/game/interfaces/iphrase';
 import {ITee} from 'src/app/modules/game/interfaces/itee';
 import {SocketService} from 'src/app/modules/game/services/socket.service';
 import {ActionService} from 'src/app/core/services/action.service';
-import {takeUntil} from 'rxjs/operators';
-import {Subject} from 'rxjs';
-import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-tee-vote-view',
@@ -20,109 +12,59 @@ import {MatSnackBar} from '@angular/material/snack-bar';
   styleUrls: ['./tee-vote-view.component.scss']
 })
 export class TeeVoteViewComponent implements OnInit {
-  public leftTeeVotes: number = 0;
-  public rightTeeVotes: number = 0;
-  public isVoted: boolean = false;
+  public hasVoted: boolean;
 
-  private notifier = new Subject();
-
-  // public pictureLeft2: {picture: IPicture, phrase: IPhrase, created_by: string};
-
-  public pictureLeft = {
-    "picture" : { 
-      "url":"https://wfh-test.s3.eu-north-1.amazonaws.com/pictures/6262/wm8sSQ7-CqWnzYKEAAAA.2.png",
-      "created_by":"wm8sSQ7-CqWnzYKEAAAA",
-      "background":"#5f0000"
-    },
-    "phrase" : {
-      "phrase":"testovaya futbolka",
-      "created_by":"8odsRkoX1XByZ5XqAAAB"
-    },
-    "created_by":"8odsRkoX1XByZ5XqAAAB"
-  };
-  public pictureRight = {
-    "picture" : {
-      "url":"https://wfh-test.s3.eu-north-1.amazonaws.com/pictures/6262/8odsRkoX1XByZ5XqAAAB.2.png",
-      "created_by":"8odsRkoX1XByZ5XqAAAB",
-      "background":"#223618"
-    },
-    "phrase" : {
-      "phrase":"vlad konechno krasava",
-      "created_by":"wm8sSQ7-CqWnzYKEAAAA"
-    },
-    "created_by":"wm8sSQ7-CqWnzYKEAAAA"
-  };
+  public leftTee: ITee;
+  public rightTee: ITee;
 
   constructor(
-    private gameViewService: GameViewService,
-
     private dataStore: DataStoreService,
-    private snackBar: MatSnackBar,
     private socketService: SocketService,
     private actionService: ActionService
   ) {}
 
   ngOnInit(): void {
-    this.dataStore.setGameStage(Stages.matching);
-    // this.listenTeeCreated();
-    // this.listenPairsCreated();
-    // this.initCreatePairs();
+    this.setVotingStage();
+    this.listenReceiveTeePair();
+    this.initVote();
+  }
+
+  private setVotingStage(): void {
+    this.dataStore.setGameStage(Stages.voting);
+    this.hasVoted = false;
   }
 
   private initVote(): void {
-    this.socketService.emit('start-vote', {room: this.dataStore.getRoomCode()});
+    this.socketService.emit('start-voting', {room: this.dataStore.getRoomCode()});
   }
 
-  private getTees() {
-    this.socketService.listen('vote-tees').subscribe(({payload}) => {
-      console.log(payload);
-      // this.pictureLeft = payload.map(item => item[0]);
-      // this.pictureRight = payload.map(item => item[1]);
+  private listenReceiveTeePair() {
+    this.socketService.listen('send-vote-tees').subscribe(({answer, payload}) => {
+      console.log(answer);
+      if (this.hasVoted) {
+        console.log('Setting new stage, since hasvoted is', this.hasVoted);
+        this.setVotingStage();
+      }
+
+      this.leftTee = payload[0];
+      this.rightTee = payload[1];
     });
   }
 
-  private sendVotes() {
-    const votedTees = [
-      {
-        votes: this.leftTeeVotes, 
-        tee: {
-          picture: this.pictureLeft.picture.url, 
-          phrase: this.pictureLeft.phrase.phrase, 
-          created_by: this.pictureLeft.created_by
-        }
-      },
-      {
-        votes: this.rightTeeVotes, 
-        tee: {
-          picture: this.pictureRight.picture.url, 
-          phrase: this.pictureRight.phrase.phrase,
-          created_by: this.pictureRight.created_by
-        }
-      }
-    ];
-
-    // this.socketService.emit('vote-result', {votedTees, room: this.dataStore.getRoomCode()});
+  public voteForLeftTee(): void {
+    this.submitVote(this.leftTee);
   }
 
-  voteForLeftPicture(): void {
-    if (this.isVoted) {
-      return;
-    }
-    this.leftTeeVotes++;
-    this.isVoted = true;
-    this.sendVotes();
+  public voteForRightTee(): void {
+    this.submitVote(this.rightTee);
   }
 
-  voteForRightPicture(): void {
-    if (this.isVoted) {
-      return;
-    }
-    this.rightTeeVotes++;
-    this.isVoted = true;
-    this.sendVotes();
-  }
+  private submitVote(winner) {
+    this.hasVoted = true;
+    const username = this.dataStore.getUserName();
+    const room = this.dataStore.getRoomCode();
 
-  handleSubmit(): void {
-    this.gameViewService.setCurrentView(views.TEE_RESULT);
+    this.socketService.emit('send-vote', {username, winner, room});
+    this.actionService.registerAction();
   }
 }
