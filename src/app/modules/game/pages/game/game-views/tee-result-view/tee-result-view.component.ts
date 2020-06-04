a84f2d546343cca0c0b661ac23162a6e16dd0bbd
Nataliia Mogylchuk <nataliia.mogylchuk@gmail.com>
2020-06-04 23:19:36 +0300

    Update tee-result-view

--- src/app/modules/game/pages/game/game-views/tee-result-view/tee-result-view.component.ts
+++ src/app/modules/game/pages/game/game-views/tee-result-view/tee-result-view.component.ts
@@ -1,20 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import { GameViewService } from 'src/app/modules/game/services/game-view.service';
-import * as views from 'src/app/modules/game/constants/game-views.js';
-
+import {Router} from '@angular/router';
 @Component({
   selector: 'app-tee-result-view',
   templateUrl: './tee-result-view.component.html',
   styleUrls: ['./tee-result-view.component.scss']
 })
 export class TeeResultViewComponent implements OnInit {
-  constructor(private gameViewService: GameViewService) {}
+
+  public teeFinal = {
+    "picture" : { 
+      "url":"https://wfh-test.s3.eu-north-1.amazonaws.com/pictures/6262/wm8sSQ7-CqWnzYKEAAAA.2.png",
+      "created_by":"wm8sSQ7-CqWnzYKEAAAA",
+      "background":"#5f0000"
+    },
+    "phrase" : {
+      "phrase":"testovaya futbolka",
+      "created_by":"8odsRkoX1XByZ5XqAAAB"
+    },
+    "created_by":"8odsRkoX1XByZ5XqAAAB"
+  };
+
+  constructor(private router: Router) {}
 
   ngOnInit(): void {}
 
-  handleSubmit(): void {
-    // TODO: add game logic to jump back to tee-voting if there are tee pairs left
-    // or go to scoreboard page / finish the game otherwise
-    this.gameViewService.setCurrentView(views.TEE_RESULT);
+  public leaveGame() {
+    this.router.navigate(['/main/welcome']);
   }
 }
